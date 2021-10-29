74f65a7badfde8c059c367e901dcc560dd1013ba
ducbe <ducbe2k2@gmail.com>
2021-10-29 08:09:00 +0700

    first commit

---
+++ resources/views/components/script.blade.php
@@ -0,0 +1,6 @@
+<script src="{{assets("assets/vendor/jquery/jquery-3.5.1.min.js")}}"></script>
+<script src="{{assets("assets/vendor/bootstrap-5/js/bootstrap.min.js")}}"></script>
+<script src="{{assets("assets/vendor/bootstrap-5/js/bootstrap.bundle.min.js")}}"></script>
+<script src="{{assets("assets/vendor/icon/Ite-ie7.js")}}"></script>
+<script src="{{assets("assets/js/script.js")}}"></script>
+
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <x-head></x-head>
+<body>
+    <main>
+        
+    </main>
+</body>
+<x-script></x-script>
+</html>
