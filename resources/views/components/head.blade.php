911920902af500b901f50d8c3813aa50cab0f90a
ducbe <ducbe2k2@gmail.com>
2021-10-29 13:23:15 +0700

    commit ui

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>RESTFUL APIs</title>
+    <title>7TH.DEC APIs</title>
     <link type="text/css" rel="stylesheet" href="{{asset("assets/vendor/bootstrap-5/css/bootstrap.min.css")}}" />
     <link type="text/css" rel="stylesheet" href="{{asset("assets/vendor/fontawesome5/css/all.min.css")}}" />
     <link type="text/css" rel="stylesheet" href="{{asset("assets/css/style.css")}}" />
