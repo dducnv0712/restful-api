14dde8c4497da76ca18ee39fe57875011dad2b24
ducbe <ducbe2k2@gmail.com>
2021-11-03 23:03:27 +0700

    commit create post, update post, delete post

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 @section('main_content')
 <div class="container">
     <div class="row justify-content-center">
-        
+
         <div class="col-md-8">
             <div class="card mt-5">
                 <div class="card-header">{{ __('Dashboard') }}</div>
@@ -15,7 +15,7 @@
                     @endif
 
                     {{ __('You are logged in!') }}
-                    <a>{{Auth::user()->user_name}}</a>
+                    <a>{{Auth::user()->user_name}} ID:{{Auth::user()->id}}</a>
                 </div>
             </div>
         </div>
